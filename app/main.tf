b34c6282f394b79545b698cfb6a722908e3525ae
Asure Sky <devops@123cloud.cl>
2021-07-23 09:32:22 -0400

    f

--- app/main.tf
+++ app/main.tf
@@ -7,5 +7,23 @@ terraform {
 }
 module "app" {
     source = "../../../app"
-    instance_type = var.instance_type
+    allocated_storage    = 20
+    engine               = "mariadb"
+    engine_version       = "10.4.13"
+    instance_class       = "db.t3.micro"
+    name                 = "mydb"
+    username             = "foo"
+    password             = "foobarbazx"
+    parameter_group_name = "default.mariadb10.4"
+    apply_immediately = true
+    skip_final_snapshot  = true
+    backup_retention_period = "10"
+    backup_window = "04:46-05:46"
+    db_subnet_group_name = "default"
+    delete_automated_backups = false
+    iam_database_authentication_enabled = false
+    identifier = "thor-production-mx"
+    multi_az = "true"
+    port = "3306"
+    publicly_accessible = false
 } 
